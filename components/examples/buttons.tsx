63f3b313e0bea6f01449e4cd14e0e9ac480e90e5
Alexisxde <olivarezalexis749@gmail.com>
2025-10-16 23:09:26 -0300

    add variant error

--- components/examples/buttons.tsx
+++ components/examples/buttons.tsx
@@ -3,7 +3,7 @@ import Button from "@/components/ui/button"
 export default function Buttons() {
 	return (
 		<div className="flex flex-wrap items-center justify-center gap-4">
-			<Button variant={"default"}>Primary</Button>
+			<Button>Primary</Button>
 			<Button variant={"secondary"}>Secondary</Button>
 			<Button variant={"ghost"}>Ghost</Button>
 			<Button variant={"outline"}>Outline</Button>
