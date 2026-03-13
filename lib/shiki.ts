c8f47a19913e639709fffae751b4ccdf7cee5067
Alexisxde <olivarezalexis749@gmail.com>
2026-03-12 21:24:43 -0300

    fix: import ButtonAsChild component and optimize highlighter initialization

--- lib/shiki.ts
+++ lib/shiki.ts
@@ -1,11 +1,17 @@
-import { bundledLanguages, createHighlighter } from "shiki/bundle/web"
+import { bundledLanguages, createHighlighter, type Highlighter } from "shiki/bundle/web"
 import { noir } from "./custom-theme"
 
+let highlighterPromise: Promise<Highlighter> | null = null
+
 export const codeToHtml = async ({ code, lang }: { code: string; lang: string }) => {
-	const highlighter = await createHighlighter({
-		themes: [noir],
-		langs: [...Object.keys(bundledLanguages)]
-	})
+	if (!highlighterPromise) {
+		highlighterPromise = createHighlighter({
+			themes: [noir],
+			langs: [...Object.keys(bundledLanguages)]
+		})
+	}
+
+	const highlighter = await highlighterPromise
 
 	return highlighter.codeToHtml(code, {
 		lang: lang,
